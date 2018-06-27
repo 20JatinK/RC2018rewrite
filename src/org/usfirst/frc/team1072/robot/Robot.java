d04218a086ffeb73ff3cb6982983454053ddd26b
20jatink <20jatink@students.harker.org>
2018-06-26 21:31:44 -0700

    Support for Gradle
    GradleRio (https://github.com/wpilibsuite/GradleRIO) is how we will deploy code to the robot and do other useful things

--- src/org/usfirst/frc/team1072/robot/Robot.java
+++ src/org/usfirst/frc/team1072/robot/Robot.java
@@ -1,10 +1,18 @@
 package org.usfirst.frc.team1072.robot;
 
 import org.usfirst.frc.team1072.subsystems.Drivetrain;
-
+import org.usfirst.frc.team1072.robot.OI;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
 	public static final Drivetrain drivetrain = Drivetrain.getInstance();
+	
+	public void robotInit() {
+		OI.initBindings();
+	}
+	
+	public void robotPeriodic() {
+		
+	}
 }
