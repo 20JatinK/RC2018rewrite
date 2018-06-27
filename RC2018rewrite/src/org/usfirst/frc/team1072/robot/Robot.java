1a0048d179c29ac0bfdb398c3840960c5fd80eef
20JatinK <20Jatink@students.harker.org>
2018-06-26 18:53:35 -0700

    What I have so far
    includes HarkerRoboLib, existing libraries, and an unfinished version of ArcadeDriveCommand, Robot class, and Drivetrain subsystem

---
+++ RC2018rewrite/src/org/usfirst/frc/team1072/robot/Robot.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1072.robot;
+
+import org.usfirst.frc.team1072.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+	public static final Drivetrain drivetrain = Drivetrain.getInstance();
+}
