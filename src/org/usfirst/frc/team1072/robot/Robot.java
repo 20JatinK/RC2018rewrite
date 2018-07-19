c5c8e67530d91fc0f982cbefe90818e18cefe6e2
FinnitoProductions <finn@teachmy.com>
2018-07-19 13:49:50 -0700

    Removed magic numbers and incorporated more constants into RobotMap

--- src/org/usfirst/frc/team1072/robot/Robot.java
+++ src/org/usfirst/frc/team1072/robot/Robot.java
@@ -27,44 +27,51 @@ public class Robot extends TimedRobot {
 	
 	public void teleopInit() {
 		//encoder setup
-		drivetrain.getLeft().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-		drivetrain.getRight().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-		elevator.getTalon().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+		drivetrain.getLeft().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 
+		        RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
+		drivetrain.getRight().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 
+		        RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
+		elevator.getTalon().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 
+		        RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
 		
 		//zero the encoder
-		drivetrain.getLeft().setSelectedSensorPosition(0, 0, 10);
-		drivetrain.getRight().setSelectedSensorPosition(0, 0, 10);
-		elevator.getTalon().setSelectedSensorPosition(0, 0, 10);
+		drivetrain.getLeft().setSelectedSensorPosition(0, RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
+		drivetrain.getRight().setSelectedSensorPosition(0, RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
+		elevator.getTalon().setSelectedSensorPosition(0, RobotMap.PRIMARY_PID_LOOP, RobotMap.TIMEOUT);
 		
 		drivetrain.getLeft().setSensorPhase(RobotMap.Drivetrain.invertedEncoders);
 		drivetrain.getRight().setSensorPhase(RobotMap.Drivetrain.invertedEncoders);
 		
-		drivetrain.getLeft().config_kP(0, RobotMap.Drivetrain.position_kP_Left, 10);
-		drivetrain.getRight().config_kP(0, RobotMap.Drivetrain.position_kP_Right, 10);
+		drivetrain.getLeft().config_kP(RobotMap.Drivetrain.positionPIDSlot, 
+		        RobotMap.Drivetrain.position_kP_Left, RobotMap.TIMEOUT);
+		drivetrain.getRight().config_kP(RobotMap.Drivetrain.positionPIDSlot, 
+		        RobotMap.Drivetrain.position_kP_Right, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().config_kI(0, RobotMap.Drivetrain.position_kI_Left, 10);
-		drivetrain.getRight().config_kI(0, RobotMap.Drivetrain.position_kI_Right, 10);
+		drivetrain.getLeft().config_kI(RobotMap.Drivetrain.positionPIDSlot, 
+		        RobotMap.Drivetrain.position_kI_Left, RobotMap.TIMEOUT);
+		drivetrain.getRight().config_kI(RobotMap.Drivetrain.positionPIDSlot, 
+		        RobotMap.Drivetrain.position_kI_Right, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().config_kD(0, RobotMap.Drivetrain.position_kD_Left, 10);
-		drivetrain.getRight().config_kD(0, RobotMap.Drivetrain.position_kD_Right, 10);
+		drivetrain.getLeft().config_kD(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_kD_Left, RobotMap.TIMEOUT);
+		drivetrain.getRight().config_kD(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_kD_Right, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().config_kF(0, RobotMap.Drivetrain.position_kF_Left, 10);
-		drivetrain.getRight().config_kF(0, RobotMap.Drivetrain.position_kF_Right, 10);
+		drivetrain.getLeft().config_kF(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_kF_Left, RobotMap.TIMEOUT);
+		drivetrain.getRight().config_kF(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_kF_Right, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().configNominalOutputForward(0.04, 10);
-		drivetrain.getRight().configNominalOutputForward(0.04, 10);
+		drivetrain.getLeft().configNominalOutputForward(RobotMap.Drivetrain.NOMINAL_OUTPUT_LEFT, RobotMap.TIMEOUT);
+		drivetrain.getRight().configNominalOutputForward(RobotMap.Drivetrain.NOMINAL_OUTPUT_RIGHT, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().configNominalOutputReverse(-0.04, 10);
-		drivetrain.getRight().configNominalOutputReverse(-0.04, 10);
+		drivetrain.getLeft().configNominalOutputReverse(-RobotMap.Drivetrain.NOMINAL_OUTPUT_LEFT, RobotMap.TIMEOUT);
+		drivetrain.getRight().configNominalOutputReverse(-RobotMap.Drivetrain.NOMINAL_OUTPUT_RIGHT, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().configPeakOutputForward(1, 10);
-		drivetrain.getRight().configPeakOutputForward(1, 10);
+		drivetrain.getLeft().configPeakOutputForward(RobotMap.Drivetrain.PEAK_OUTPUT_LEFT, RobotMap.TIMEOUT);
+		drivetrain.getRight().configPeakOutputForward(RobotMap.Drivetrain.PEAK_OUTPUT_RIGHT, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().configPeakOutputReverse(-1, 10);
-		drivetrain.getRight().configPeakOutputReverse(-1, 10);
+		drivetrain.getLeft().configPeakOutputReverse(-RobotMap.Drivetrain.PEAK_OUTPUT_LEFT, RobotMap.TIMEOUT);
+		drivetrain.getRight().configPeakOutputReverse(-RobotMap.Drivetrain.NOMINAL_OUTPUT_RIGHT, RobotMap.TIMEOUT);
 		
-		drivetrain.getLeft().config_IntegralZone(RobotMap.Drivetrain.positionPIDSlot, 250, 10);
-		drivetrain.getRight().config_IntegralZone(RobotMap.Drivetrain.positionPIDSlot, 250, 10);
+		drivetrain.getLeft().config_IntegralZone(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_iZone_Left, RobotMap.TIMEOUT);
+		drivetrain.getRight().config_IntegralZone(RobotMap.Drivetrain.positionPIDSlot, RobotMap.Drivetrain.position_iZone_Right, RobotMap.TIMEOUT);
 	}
 	
 	public void teleopPeriodic() {
